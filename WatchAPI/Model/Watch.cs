abc15b10bf747c60bbe0ddfc3fa29ec6e612bbff
aldrinalmia <aldrin.almia@gmail.com>
2023-07-25 21:13:57 +0800

    modify

--- WatchAPI/Model/Watch.cs
+++ WatchAPI/Model/Watch.cs
@@ -10,49 +10,59 @@ namespace WatchAPI.Model
     {
         [Key]
         public int ID { get; set; }
-
         [Required]
         [StringLength(50)]
         public string? ItemName { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         [StringLength(50)]
         public string? ItemNumber { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(150)]
         public string? ShortDescription { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(500)]
         public string? FullDescription { get; set; }
-        [Required]
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid price (e.g. 5000.00)")]
+        [Range(5000, 5000000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Required(ErrorMessage = "Required")]
         public decimal Price { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         [StringLength(50)]
         public string? Caliber { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         [StringLength(50)]
         public string? Movement { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
+        [StringLength(150)]
         public string? Chronograph { get; set; }
-        [Required]
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid weight (e.g. 20.00)")]
+        [Range(1, 300, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Required(ErrorMessage = "Required")]
         public decimal Weight { get; set; }
-        [Required]
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid height (e.g. 20.00)")]
+        [Range(1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Required(ErrorMessage = "Required")]
         public decimal Height { get; set; }
-        [Required]
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid diameter (e.g. 20.00)")]
+        [Range(1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Required(ErrorMessage = "Required")]
         public decimal Diameter { get; set; }
-        [Required]
+        [RegularExpression(@"-?\d+(?:\.\d+)?", ErrorMessage = "Please enter valid thickness (e.g. 20.00)")]
+        [Range(1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Required(ErrorMessage = "Required")]
         public decimal Thickness { get; set; }
-        [Required]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Please enter numbers only")]
+        [Range(1, 100, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        [Required(ErrorMessage = "Required")]
         public int Jewel { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         [StringLength(30)]
         public string? CaseMaterial { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required")]
         [StringLength(30)]
         public string? StrapMaterial { get; set; }
-
-        //[JsonIgnore]
         [NotMapped]
-        [Display(Name = "File")]
         public IFormFile? FormFile { get; set; }
-
         public string? URL { get; set; }
     }
 }
